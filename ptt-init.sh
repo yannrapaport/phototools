faa0d16f2a55655724f7ed8d2e294f4e57d5028f
Yann Rapaport <yann.rapaport@gmail.com>
2018-05-06 17:31:03 +0200

    number folders

--- ptt-init.sh
+++ ptt-init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PICTURES="*.DNG *.JPG"
 MOVIES="*.MOV"
-mkdir capture master select output video
-mv $PICTURES capture
-mv $MOVIES video
+mkdir 1-capture 2-select 3-master 4-output 10-video
+mv $PICTURES 1-capture
+mv $MOVIES 10-video
